567ea29a32e03baa9590008e1a9b0f4724116e49
rantiindriyani <121720015+rantiindriyani@users.noreply.github.com>
2023-05-20 21:14:42 +0700

    Add files via upload

---
+++ Node1.java
@@ -0,0 +1,14 @@
+public class Node1 {
+    private int data;
+    
+    public Node1(int key){
+        data=key;
+    }
+    public int getKey(){
+        return data;
+    }
+    public void setKey(int id){
+        data=id;
+    }
+}
+
